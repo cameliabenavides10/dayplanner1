e09a06a8ba8a03f6ee8e647244c8ac43f272c275
cameliabenavides10 <cameliabenavides@gmail.com>
2023-01-07 22:42:17 -0600

    updated

--- assets/script.js
+++ assets/script.js
@@ -34,15 +34,44 @@ setInterval(intervalHeaderTime, 1000);
 
 
 // The following will be in order to change the colors on the planner depending on time of day.
+// var intervalCurrentHour = function() {
 // var currentHour = dayjs();
-//     $('#currentDay').text(currentHour.format('H'));
-var timeGroups = ["9", "10", "11", "12", "13", "14", "15", "16","17"]
- for (let i=0; i < timeGroups.length; i++){
-if (currentHour === timeGroups){
-    // set to present color
-} else if(currentHour > timeGroups){
-    // set to past color
-} else { 
-    // set to future color
-}
- }
+// i dont want to append it anywhere but im not sure how to write it yet
+//  $('#currentDay').text(currentHour.format('h A'));
+// };
+// setInterval(intervalCurrentHour, 1000);
+// console.log(intervalCurrentHour);
+// var timeGroups = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM","5 PM"]
+// var hourBox = document.querySelector(".time-block");
+// do another for loop and see if you can select the ids and append them to a blank variable that will equal timegroups
+
+var nine = $("#hour-9").text();
+var ten = $("#hour-10").text();
+var eleven= $("#hour-11").text();
+var twelve= $("#hour-12").text(); 
+var one= $("#hour-13").text();
+var two= $("hour-14").text();
+var three= $("#hour-15").text();
+var four=$("#hour-16").text();
+var five= $("#hour-17").text();
+
+
+var theHours = [nine, ten, eleven, twelve, one, two, three, four, five];
+
+console.log(theHours[0]);
+
+
+
+// for looping the objects in 
+
+
+// wait can you make a for loop with an object??
+//  for (let i=0; i < theHours.length; i++){
+// if (currentHour === timeGroups){
+//     // set to present color using the style thing? 
+// } else if(currentHour > timeGroups){
+//     // set to past color 
+// } else { 
+//     // set to future color
+// }
+//  }
